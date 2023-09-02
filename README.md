0a90e7c004788f5ee20d46c1a067a5dde49e469f
Sadiq Adedayo <adedayoas91@gmail.com>
2023-09-02 11:04:57 +0200

    ran pycodestyle and fixed minor bugs

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# Udacity-AdventueGame
-Python Adventure Game 
+# Adventue Game
+
+## Python Adventure Game 
+
+Implementation of an adventure game which entails a player who chooses a random weapon from a weapon list to fight against a monster.
+
+Enjoy!!!
--- adventure.py
+++ adventure.py
@@ -2,31 +2,43 @@ import time
 import random
 
 
-class GameColor:
+class PrintColor:
     COLORS = ["\033[91m", "\033[95m", "\033[94m", "\033[96m", "\033[92m"]
 
     @classmethod
     def get_color(cls):
+        """
+        Choose colors randomly to be used for printing texts in the console
+        """
         return random.choice(cls.COLORS)
 
 
-def print_colored(message, delay=2):
-    color_message = f"{GameColor.get_color()}{message}\033[0m"
-    print(color_message)
+def print_colored(text, delay=2):
+    """
+    Function that calls get_color() method and displays
+    texts in the chosen color
+    Args
+    :param text: (str) message to be displayed on the console
+    :param delay: (int) The number of seconds delay before
+    another text is printed on the console
+    """
+    color_text = f"{PrintColor.get_color()}{text}\033[0m"
+    print(color_text)
     time.sleep(delay)
 
 
-def get_user_choice(prompt, options):
+def get_user_decision(prompt, options):
     while True:
         response = input(prompt).lower()
         if response in options:
             return response
         print("Invalid input. Try again.")
 
+
 def battle_sequence(battle_texts):
     for text in battle_texts:
-        print_colored(text, 1)
-    print_colored("\n", 1)
+        print_colored(text, 2)
+    print_colored("\n", 2)
 
 
 def battle_text():
@@ -38,8 +50,9 @@ def battle_text():
     ]
 
 
-def fight_or_flee():
-    decision = get_user_choice("Choose 1 to fight or 2 to flee: ", ["1", "2"])
+def escape_or_fight():
+    decision = get_user_decision("Choose 1 to fight or 2 "
+                                 "to flee: ", ["1", "2"])
     if decision == "1":
         battle_sequence(battle_text())
         return True
@@ -48,23 +61,24 @@ def fight_or_flee():
         return False
 
 
-def house_encounter(enemy, weapon):
-    print_colored(f"A {enemy} emerges from the house.", 2)
-    print_colored(f"The {enemy} attacks! Prepare to defend yourself.\n")
+def house_encounter(deamon, weapon):
+    print_colored(f"A {deamon} emerges from the house.", 2)
+    print_colored(f"The {deamon} attacks! Prepare to defend yourself.\n")
 
-    if weapon == "badKnife":
+    if weapon == "small dagger":
         print_colored("You feel unprepared with just a small dagger.", 2)
-        choice = fight_or_flee()
+        decision = escape_or_fight()
     elif weapon == "newKnife":
-        choice = fight_or_flee()
-    return choice
+        decision = escape_or_fight()
+    return decision
 
 
 def explore_cave(weapon):
-    if weapon == "badKnife":
+    if weapon == "small dagger":
         print_colored("You enter the cave and spot a gleam of metal.", 2)
         print_colored("You've discovered the magical new knife!", 2)
-        print_colored("You discard your old dagger and take the new weapon.", 2)
+        print_colored("You discard your old dagger and take the "
+                      "new weapon.", 2)
         weapon = "newKnife"
     elif weapon == "newKnife":
         print_colored("You enter the cave but find it empty.", 2)
@@ -72,39 +86,47 @@ def explore_cave(weapon):
     return weapon
 
 
-def game_intro(enemy):
+def game_opener(deamon):
     print_colored("Welcome to an adventure in a mystical land!", 2)
-    print_colored("You're in an open field with grass and wildflowers.", 2)
-    print_colored(f"A {enemy} is rumored to be nearby, terrorizing the village.", 2)
-    print_colored("To your right is a dark cave, and ahead is a mysterious house.", 2)
+    print_colored("You're in an open field with grass and "
+                  "wildflowers.", 2)
+    print_colored(f"A {deamon} is rumored to be nearby, "
+                  f"terrorizing the village.", 2)
+    print_colored("To your right is a dark cave, and ahead is "
+                  "a mysterious house.", 2)
     print_colored("You hold a dagger, ready to face any challenges.\n", 2)
 
 
 def play_adventure(creatures, weapon):
     while True:
-        print_colored("Enter 1 to approach the house, or 2 to explore the cave.", 2)
-        action = get_user_choice("What's your choice? (Enter 1 or 2): ", ["1", "2"])
+        print_colored("Enter 1 to approach the house, or 2 "
+                      "to explore the cave.", 2)
+        action = get_user_decision("What's your decision? "
+                                   "(Enter 1 or 2): ", ["1", "2"])
 
         if action == "1":
-            choice = house_encounter(creatures, weapon)
-            if choice:
+            decision = house_encounter(creatures, weapon)
+            if decision:
                 break
         elif action == "2":
             weapon = explore_cave(weapon)
 
 
 def main_game():
-    weapon = "badKnife"
-    enemies = ["medusa", "pirate", "lion", "wicked mermaid"]
+    weapon = "small dagger"
+    deamon_list = ["medusa", "pirate", "lion", "wicked mermaid"]
 
     while True:
-        enemy = random.choice(enemies)
-        game_intro(enemy)
-        play_adventure(enemy, weapon)
+        deamon = random.choice(deamon_list)
+        game_opener(deamon)
+        play_adventure(deamon, weapon)
 
-        replay = get_user_choice("Play again? (y/n): ", ["y", "n"])
+        replay = get_user_decision("Would you like to play again? "
+                                   "(y/n): ", ["y", "n"])
         if replay == "n":
-            print_colored("Thanks for playing! Until next time.", 2)
+            print_colored("🤨🤨🤨")
+            print_colored("Thanks for playing! Looking forward to "
+                          "your coming back for more🤗", 2)
             break
         elif replay == "y":
             print_colored("Restarting the adventure...\n", 2)
